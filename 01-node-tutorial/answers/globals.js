b8e5efe59b5020dfdb053f9928a735e955fe945a
Vladimir <99782728+vpostyka@users.noreply.github.com>
2023-06-03 12:14:21 -0700

    added .env

--- 01-node-tutorial/answers/globals.js
+++ 01-node-tutorial/answers/globals.js
@@ -1,7 +1,12 @@
-console.log('__dirname: ', __dirname);
+require('dotenv').config();
+//import 'dotenv/config'
+
+
+//console.log('__dirname: ', __dirname);
 console.log('MY_VAR: ', process.env.MY_VAR);
+//console.log(process.env);
 
 // Let's log some other globals for the sake of it
-console.log('__filename: ', __filename);
-console.log('Platform: ', process.platform);
-console.log('Node Version: ', process.version);
+// console.log('__filename: ', __filename);
+// console.log('Platform: ', process.platform);
+// console.log('Node Version: ', process.version);
