833b4c16c63285b220629973eedf487015381ec8
Vladimir <99782728+vpostyka@users.noreply.github.com>
2023-06-04 19:30:48 -0700

    fixed env variables

--- 01-node-tutorial/answers/globals.js
+++ 01-node-tutorial/answers/globals.js
@@ -1,12 +1,7 @@
-require('dotenv').config();
-//import 'dotenv/config'
-
-
-//console.log('__dirname: ', __dirname);
+console.log('__dirname: ', __dirname);
 console.log('MY_VAR: ', process.env.MY_VAR);
 //console.log(process.env);
 
-// Let's log some other globals for the sake of it
-// console.log('__filename: ', __filename);
-// console.log('Platform: ', process.platform);
-// console.log('Node Version: ', process.version);
+console.log('__filename: ', __filename);
+console.log('Platform: ', process.platform);
+console.log('Node Version: ', process.version);
