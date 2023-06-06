03bf646c0b037d47db265d1b9c14847267814f3e
Vladimir <99782728+vpostyka@users.noreply.github.com>
2023-06-05 20:07:46 -0700

    added flags

--- 01-node-tutorial/answers/fs-async.js
+++ 01-node-tutorial/answers/fs-async.js
@@ -1,19 +1,35 @@
-const {writeFile, appendFile} = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const filePath = path.join(__dirname, 'temporary', 'fileB.txt');
+const filePath = path.join(__dirname, "temporary", "fileB.txt");
 
-writeFile(filePath, 'This is line 1\n', 'utf8', (err) => {
+fs.writeFile(filePath, "This is line 1\n", "utf8", (err) => {
   if (err) throw err;
-  console.log('Line 1 written');
+  console.log("Line 1 written");
 
-  appendFile(filePath, 'This is line 2\n', 'utf8', (err) => {
-    if (err) throw err;
-    console.log('Line 2 written');
-
-    appendFile(filePath, 'This is line 3\n', 'utf8', (err) => {
+  fs.writeFile(
+    filePath,
+    "This is line 2\n",
+    {
+      encoding: "utf8",
+      flag: "a",
+    },
+    (err) => {
       if (err) throw err;
-      console.log('Line 3 written');
-    });
-  });
+      console.log("Line 2 written");
+
+      fs.writeFile(
+        filePath,
+        "This is line 3\n",
+        {
+          encoding: "utf8",
+          flag: "a",
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Line 3 written");
+        }
+      );
+    }
+  );
 });
