b3c5085b389c994a117ec34f08be7330f3143820
Vladimir <99782728+vpostyka@users.noreply.github.com>
2023-06-22 21:26:58 -0700

    added tska for week4

--- 02-express-tutorial/public/index.js
+++ 02-express-tutorial/public/index.js
@@ -9,3 +9,15 @@ document.getElementById("get-products").addEventListener("click", async () => {
     productsDiv.appendChild(productDiv);
   });
 });
+
+document.getElementById("get-people").addEventListener("click", async () => {
+  const response = await fetch("api/v1/people");
+  const products = await response.json();
+  const productsDiv = document.getElementById("people-list");
+  productsDiv.innerHTML = "";
+  products.forEach((people) => {
+    const productDiv = document.createElement("div");
+    productDiv.textContent = `People ID: ${people.id}, Name: ${people.name}`;
+    productsDiv.appendChild(productDiv);
+  });
+});
